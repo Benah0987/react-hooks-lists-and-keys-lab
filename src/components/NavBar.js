378639c034a790a0ced44b3913e4adb2620976e9
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-27 08:53:45 +0300

    learned on how to use maps and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+//map to iterate over the links
+//return an <a> element for each link
+//pass a unique key to each element
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map((link, index) =>(
+    <a key={index} href = {`#${link}`}>
+      {link}
+    </a>
+  ))}
+     
+
+
+       </nav>;
 }
 
 export default NavBar;
